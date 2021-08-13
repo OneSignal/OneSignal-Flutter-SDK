13514bc43555c41c6a230615f45c4144097420c8
Efra Espada <efraespada@gmail.com>
2021-08-13 16:54:25 +0200

    feature: using same function for defining same android / ios callack

--- lib/onesignal_flutter.dart
+++ lib/onesignal_flutter.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:io';
 import 'dart:ui';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -173,6 +174,10 @@ class OneSignal {
 
   void setNotificationWillShowHandler(
       NotificationWillShowInForegroundHandler handler) {
+    if (Platform.isIOS) {
+      setNotificationWillShowInForegroundHandler(handler);
+      return;
+    }
     final CallbackHandle bgHandle =
         PluginUtilities.getCallbackHandle(_callbackDispatcher)!;
     final CallbackHandle notificationHandler =
