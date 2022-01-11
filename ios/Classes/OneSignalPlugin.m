c37f15f47efd24b4e0c3f9d640d295ea3ff27ec7
Slawomir Krol <skkrolu@gmail.com>
2022-01-11 15:43:18 +0100

    Add missing fallbackToSettings argument on native iOS side

--- ios/Classes/OneSignalPlugin.m
+++ ios/Classes/OneSignalPlugin.m
@@ -202,9 +202,10 @@
 }
 
 - (void)promptPermission:(FlutterMethodCall *)call withResult:(FlutterResult)result {
+    BOOL fallbackToSettings = [call.arguments[@"fallback"] boolValue];
     [OneSignal promptForPushNotificationsWithUserResponse:^(BOOL accepted) {
         result(@(accepted));
-    }];
+    } fallbackToSettings:fallbackToSettings];
 }
 
 - (void)getDeviceState:(FlutterMethodCall *)call withResult:(FlutterResult)result {
