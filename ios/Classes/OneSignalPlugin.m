177e31955548b3462b5d5e4c77b9361d8b414241
Nan <nanftw@gmail.com>
2023-09-27 17:18:58 -0700

    release commit 5.0.2
    * Bump SDK version number to 5.0.2
    * Update Android SDK to 5.0.2
    * iOS has been updated to 5.0.2 in a previous PR

--- ios/Classes/OneSignalPlugin.m
+++ ios/Classes/OneSignalPlugin.m
@@ -57,7 +57,7 @@
 
     [OneSignal initialize:nil withLaunchOptions:nil];
     OneSignalWrapper.sdkType = @"flutter";
-    OneSignalWrapper.sdkVersion = @"050001";
+    OneSignalWrapper.sdkVersion = @"050002";
     
     OneSignalPlugin.sharedInstance.channel = [FlutterMethodChannel
                                      methodChannelWithName:@"OneSignal"
