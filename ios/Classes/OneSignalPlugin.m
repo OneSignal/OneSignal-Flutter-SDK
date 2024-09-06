c3649824a1352a7c724d89d305df59584bafa4b5
Jenna Antilla <46546946+jennantilla@users.noreply.github.com>
2024-09-06 10:29:24 -0700

    release commit

--- ios/Classes/OneSignalPlugin.m
+++ ios/Classes/OneSignalPlugin.m
@@ -57,7 +57,7 @@
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
 
     OneSignalWrapper.sdkType = @"flutter";
-    OneSignalWrapper.sdkVersion = @"050204";
+    OneSignalWrapper.sdkVersion = @"050205";
     [OneSignal initialize:nil withLaunchOptions:nil];
 
     OneSignalPlugin.sharedInstance.channel = [FlutterMethodChannel
