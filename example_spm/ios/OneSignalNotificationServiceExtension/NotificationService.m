485bb5dfe3bbf292526c9b20a49d16d6839a015e
Fadi George <fadii925@gmail.com>
2026-01-06 16:20:52 -0800

    add example projects for CocoaPods and SPM

---
+++ example_spm/ios/OneSignalNotificationServiceExtension/NotificationService.m
@@ -0,0 +1,40 @@
+//
+//  NotificationService.m
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Brad Hesse on 7/13/18.
+//  Copyright © 2018 The Chromium Authors. All rights reserved.
+//
+
+#import <OneSignalFramework/OneSignalFramework.h>
+
+#import "NotificationService.h"
+
+@interface NotificationService ()
+
+@property (nonatomic, strong) void (^contentHandler)(UNNotificationContent *contentToDeliver);
+@property (nonatomic, strong) UNNotificationRequest *receivedRequest;
+@property (nonatomic, strong) UNMutableNotificationContent *bestAttemptContent;
+
+@end
+
+@implementation NotificationService
+
+- (void)didReceiveNotificationRequest:(UNNotificationRequest *)request withContentHandler:(void (^)(UNNotificationContent * _Nonnull))contentHandler {
+    self.receivedRequest = request;
+    self.contentHandler = contentHandler;
+    self.bestAttemptContent = [request.content mutableCopy];
+    
+    [OneSignal didReceiveNotificationExtensionRequest:self.receivedRequest withMutableNotificationContent:self.bestAttemptContent withContentHandler:self.contentHandler];
+}
+
+- (void)serviceExtensionTimeWillExpire {
+    // Called just before the extension will be terminated by the system.
+    // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
+    
+    [OneSignal serviceExtensionTimeWillExpireRequest:self.receivedRequest withMutableNotificationContent:self.bestAttemptContent];
+    
+    self.contentHandler(self.bestAttemptContent);
+}
+
+@end
