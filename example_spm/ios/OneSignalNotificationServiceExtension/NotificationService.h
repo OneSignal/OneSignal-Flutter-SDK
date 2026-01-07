cf7435d183708473a77abc350c4a78b93a372643
Fadi George <fadii925@gmail.com>
2026-01-07 09:52:21 -0800

    chore: adds example projects for CocoaPods and SPM (#1105)

---
+++ example_spm/ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,13 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Brad Hesse on 7/13/18.
+//  Copyright © 2018 The Chromium Authors. All rights reserved.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
