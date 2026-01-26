ca631f29527e393a7c00d95543e7b6637ec9694f
Fadi George <fadii925@gmail.com>
2026-01-26 15:31:30 -0800

    chore: Release 5.3.7 (#1112)
    Co-authored-by: github-actions[bot] <noreply@onesignal.com>

--- ios/Classes/OneSignalPlugin.m
+++ ios/Classes/OneSignalPlugin.m
@@ -56,7 +56,7 @@
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar> *)registrar {
 
   OneSignalWrapper.sdkType = @"flutter";
-  OneSignalWrapper.sdkVersion = @"050306";
+  OneSignalWrapper.sdkVersion = @"050307";
   [OneSignal initialize:nil withLaunchOptions:nil];
 
   OneSignalPlugin.sharedInstance.channel =
