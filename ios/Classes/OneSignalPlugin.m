82dab851491d2cca6d1e705f9c55c2d12d89e698
onesignal-deploy <deploy@onesignal.com>
2026-03-13 10:26:26 -0700

    chore: Release 5.4.4 (#1127)
    Co-authored-by: github-actions[bot] <noreply@onesignal.com>

--- ios/Classes/OneSignalPlugin.m
+++ ios/Classes/OneSignalPlugin.m
@@ -56,7 +56,7 @@
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar> *)registrar {
 
   OneSignalWrapper.sdkType = @"flutter";
-  OneSignalWrapper.sdkVersion = @"050403";
+  OneSignalWrapper.sdkVersion = @"050404";
   [OneSignal initialize:nil withLaunchOptions:nil];
 
   OneSignalPlugin.sharedInstance.channel =
