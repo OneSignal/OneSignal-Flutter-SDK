da08a0b437b30fac34e777a520a50d1fe4bfbbd5
onesignal-deploy <deploy@onesignal.com>
2026-03-24 14:46:14 -0700

    chore: Release 5.4.6 (#1130)
    Co-authored-by: github-actions[bot] <noreply@onesignal.com>
    Co-authored-by: Fadi George <fadii925@gmail.com>

--- android/src/main/java/com/onesignal/flutter/OneSignalPlugin.java
+++ android/src/main/java/com/onesignal/flutter/OneSignalPlugin.java
@@ -24,7 +24,7 @@ public class OneSignalPlugin extends FlutterMessengerResponder
         this.messenger = messenger;
         OneSignalWrapper.setSdkType("flutter");
         // For 5.0.0, hard code to reflect SDK version
-        OneSignalWrapper.setSdkVersion("050405");
+        OneSignalWrapper.setSdkVersion("050406");
 
         channel = new MethodChannel(messenger, "OneSignal");
         channel.setMethodCallHandler(this);
