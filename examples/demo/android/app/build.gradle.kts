5249957e9cde4f31b973ba53b0e98e3fb668aa43
Fadi George <fadii925@gmail.com>
2026-09-24 16:54:34 -0700

    test: [SDK-5300] make Google services optional

--- examples/demo/android/app/build.gradle.kts
+++ examples/demo/android/app/build.gradle.kts
@@ -3,7 +3,10 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
-    id("com.google.gms.google-services")
+}
+
+if (file("google-services.json").exists()) {
+    apply(plugin = "com.google.gms.google-services")
 }
 
 android {
